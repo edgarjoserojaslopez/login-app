3ccdd305a5e922843fe656c23e5ad3d13cc8ea75
edgarjoserojaslopez <ejrl64@yahoo.com>
2021-04-04 17:57:22 -0400

    Commit 04042021-1757

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Home from "./components/Home.component";
+import Nav from "./components/Nav.component";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Home />
+        </div>
+      </div>
     </div>
   );
 }
